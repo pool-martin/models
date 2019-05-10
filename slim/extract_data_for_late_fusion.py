9bd64b70693eb02eacf2f986d25519e0519cab7d
joao paulo <pool.martin@gmail.com>
2019-05-10 00:29:00 -0300

    fix

--- slim/extract_data_for_late_fusion.py
+++ slim/extract_data_for_late_fusion.py
@@ -104,9 +104,9 @@ start = su.print_and_time('Extract for finetune experiment ...\n', past=start, f
 
 
 input_training = os.path.join('/Exp/2kporn/art/inception_v4', FLAGS.input_split, 'finetune/svm.features')
-ids_train, labels_train, features_train, probs_train = su.read_pickled_data(os.path.join(input_training, 'feats.train'))
-ids_val, labels_val, features_val, probs_val = su.read_pickled_data(os.path.join(input_training, 'feats.validation'))
-ids_test, labels_test, features_test, probs_test = su.read_pickled_data(os.path.join(input_training, 'feats.test'))
+ids_train, labels_train, features_train = su.read_pickled_data(os.path.join(input_training, 'feats.train'))
+ids_val, labels_val, features_val = su.read_pickled_data(os.path.join(input_training, 'feats.validation'))
+ids_test, labels_test, features_test = su.read_pickled_data(os.path.join(input_training, 'feats.test'))
 
 start = su.print_and_time('', past=start, file=sys.stderr)
 ids = np.append(ids_train, ids_val)
