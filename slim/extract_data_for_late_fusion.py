d5914c7dd3fc269438da9bb5c4b5177d4fed4adc
joao paulo <pool.martin@gmail.com>
2019-05-10 00:24:43 -0300

    fix

--- slim/extract_data_for_late_fusion.py
+++ slim/extract_data_for_late_fusion.py
@@ -37,7 +37,6 @@ os.environ['JOBLIB_TEMP_FOLDER'] = "~/tmp"
 
 parser = argparse.ArgumentParser(prog='train_svm_layer.py', description='Predict the SVM decision.')
 parser.add_argument('--input_model', type=str, required=True, help='input trained model, in pickle format.')
-parser.add_argument('--input_training', type=str, required=True, help='input file with the training data, in pickle format.')
 parser.add_argument('--output_predictions', type=str , help='input file with the test data, in isbi challenge format (default=stdout).')
 parser.add_argument('--input_split', type=str, required=True, help='input split to extract.')
 
@@ -55,7 +54,7 @@ first = start = su.print_and_time('Reading train data...', file=sys.stderr)
 input_training = os.path.join('/Exp/2kporn/art/inception_v4/', FLAGS.input_split, '/saliency/svm.features')
 ids_train, labels_train, features_train, probs_train = su.read_pickled_data(os.path.join(input_training, 'feats.train'))
 ids_val, labels_val, features_val, probs_val = su.read_pickled_data(os.path.join(input_training, 'feats.validation'))
-ids_test, labels_test, features_test, probs_test = su.read_pickled_data(os.path.join(FLAGS.input_training, 'feats.test'))
+ids_test, labels_test, features_test, probs_test = su.read_pickled_data(os.path.join(input_training, 'feats.test'))
 
 start = su.print_and_time('', past=start, file=sys.stderr)
 image_ids = np.append(ids_train, ids_val)
@@ -107,7 +106,7 @@ start = su.print_and_time('Extract for finetune experiment ...\n', past=start, f
 input_training = os.path.join('/Exp/2kporn/art/inception_v4/', FLAGS.input_split, '/finetune/svm.features')
 ids_train, labels_train, features_train, probs_train = su.read_pickled_data(os.path.join(input_training, 'feats.train'))
 ids_val, labels_val, features_val, probs_val = su.read_pickled_data(os.path.join(input_training, 'feats.validation'))
-ids_test, labels_test, features_test, probs_test = su.read_pickled_data(os.path.join(FLAGS.input_training, 'feats.test'))
+ids_test, labels_test, features_test, probs_test = su.read_pickled_data(os.path.join(input_training, 'feats.test'))
 
 start = su.print_and_time('', past=start, file=sys.stderr)
 ids = np.append(ids_train, ids_val)
