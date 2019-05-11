3256ef13ecf2800c83d3148aa602d5540408be27
joao paulo <pool.martin@gmail.com>
2019-05-10 23:38:18 -0300

    train join using inner

--- slim/extract_data_for_late_fusion.py
+++ slim/extract_data_for_late_fusion.py
@@ -160,7 +160,7 @@ df2 = df2.sort_values(by='Frame')
 df2 = df2.drop_duplicates(subset='Frame')
 
 
-dfjoined = df.set_index('Frame', drop=False).join(df2.set_index('Frame', drop=False), lsuffix='_saliency', rsuffix='_finetune')
+dfjoined = df.set_index('Frame', drop=False).join(df2.set_index('Frame', drop=False), on='Frame', how='inner', lsuffix='_saliency', rsuffix='_finetune')
 # dfjoined.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
 
 print('\n joined', end='', file=sys.stderr)
